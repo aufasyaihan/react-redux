8fbf9df5f3f0502feaada1ee4c9a851773496b78
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-10 16:05:34 +0700

    added increment adn decrement function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,25 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>
-        <button>-</button>
-        <div>{counter}</div>
-        <button>+</button>
+        <button onClick={decrementHandler}>-</button>
+        {counter}
+        <button onClick={incrementHandler}>+</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
